31c1b881b195a7d24acbe9bb239565d78ca89972
vsoftic <vsoftic@ranorex.com>
2016-06-17 09:11:18 +0200

    added emphasize directive

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { OAuthService} from 'angular2-oauth2/oauth-service';
+import { Emphasize } from '../shared/emphasize.directive';
 
 @Component({
     selector: 'home',
-    template: require('./home.component.html')
+    template: require('./home.component.html'),
+    directives: [Emphasize]
 })
 export class HomeComponent {
 
